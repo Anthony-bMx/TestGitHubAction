a275087ad6d08b231d5b0f054b0b1bed6a4d8d8c
Anthony Dury <70208011+Anthony-bMx@users.noreply.github.com>
2020-12-04 16:36:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 echo "Hello $1"
 time=$(date)
 
+# Create directory if do not already exists
+mkdir -p backups
+cd backups
+
 # Example:  NotifyExport-2020-12-04_16-34-13.json
 filename=$(printf 'NotifyExport-%(%Y-%m-%d_%H-%M-%S.json)T\n' -1)
 wget https://jsonplaceholder.typicode.com/todos/1 -O $filename
