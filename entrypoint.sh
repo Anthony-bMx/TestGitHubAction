17a05792129ddc621dec9f889df045823565825f
Anthony Dury <70208011+Anthony-bMx@users.noreply.github.com>
2020-12-04 16:14:01 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo "Hello $1"
 time=$(date)
-wget https://jsonplaceholder.typicode.com/todos/1 -O file.json 
+wget https://jsonplaceholder.typicode.com/todos/1 -O downloadedfile.json 
 echo "::set-output name=time::$time"
