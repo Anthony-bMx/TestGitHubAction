c67d23bf119b3de1486a22f958ce717284f2a589
Anthony Dury <70208011+Anthony-bMx@users.noreply.github.com>
2020-12-04 16:35:15 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,10 @@
 
 echo "Hello $1"
 time=$(date)
-wget https://jsonplaceholder.typicode.com/todos/1 -O downloadedfile.json 
+
+# Example:  NotifyExport-2020-12-04_16-34-13.json
+filename=$(printf 'NotifyExport-%(%Y-%m-%d_%H-%M-%S.json)T\n' -1)
+wget https://jsonplaceholder.typicode.com/todos/1 -O $filename
+
+#wget https://jsonplaceholder.typicode.com/todos/1 -O downloadedfile.json 
 echo "::set-output name=time::$time"
